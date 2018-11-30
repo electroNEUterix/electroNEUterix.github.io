4d0a32c4cd28dadff51be86604cba3aae9b3157a
electroNEUterix <45267952+electroNEUterix@users.noreply.github.com>
2018-11-30 09:46:39 +0800

    Add files via upload

---
+++ EasyControl/js/index.js
@@ -0,0 +1,36 @@
+// on off button
+var onoffbutton;
+function OnOff() {
+   var element = document.getElementById("test");
+   element.classList.toggle("on");
+}
+
+var onoffbuttonp;
+function OnOffp() {
+   var element = document.getElementById("plant");
+   element.classList.toggle("onp");
+}
+
+var onoffbuttonp2;
+function OnOffp2() {
+   var element = document.getElementById("plant2");
+   element.classList.toggle("onp2");
+}
+
+var onoffbuttonp3;
+function OnOffp3() {
+   var element = document.getElementById("plant3");
+   element.classList.toggle("onp3");
+}
+
+var onoffbuttonp4;
+function OnOffp4() {
+   var element = document.getElementById("plant4");
+   element.classList.toggle("onp4");
+}
+
+var onoffbuttonp5;
+function OnOffp5() {
+   var element = document.getElementById("plant5");
+   element.classList.toggle("onp5");
+}

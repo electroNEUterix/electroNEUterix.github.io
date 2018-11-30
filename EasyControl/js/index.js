93876926959f7df47951284a5578409ffac68b00
electroNEUterix <45267952+electroNEUterix@users.noreply.github.com>
2018-11-30 23:39:57 +0800

    Update index.js

--- EasyControl/js/index.js
+++ EasyControl/js/index.js
@@ -1,34 +1,3 @@
-// on off button
-var onoffbutton;
-function OnOff() {
-   var element = document.getElementById("test");
-   element.classList.toggle("on");
-}
-
-var onoffbuttonp;
-function OnOffp() {
-   var element = document.getElementById("plant");
-   element.classList.toggle("onp");
-}
-
-var onoffbuttonp2;
-function OnOffp2() {
-   var element = document.getElementById("plant2");
-   element.classList.toggle("onp2");
-}
-
-var onoffbuttonp3;
-function OnOffp3() {
-   var element = document.getElementById("plant3");
-   element.classList.toggle("onp3");
-}
-
-var onoffbuttonp4;
-function OnOffp4() {
-   var element = document.getElementById("plant4");
-   element.classList.toggle("onp4");
-}
-
 
 //cpf code
 var pinreset;

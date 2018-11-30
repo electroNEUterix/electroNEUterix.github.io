044131e1edd424538a16a36e87ef0ee63b25e79d
electroNEUterix <45267952+electroNEUterix@users.noreply.github.com>
2018-12-01 05:58:30 +0800

    Update index.js

--- EasyControl/js/index.js
+++ EasyControl/js/index.js
@@ -1,12 +1,11 @@
 
 //cpf code
-function setup(){
-		if(cpf)
-cpf.pinreset("[\"resetPin\"],[\"setPinMode\", \"analog\", 0,\"INPUT\"],[\"setPinMode\", \"digital\", 2,\"OUTPUT\"],[\"setPinMode\", \"digital\", 3,\"OUTPUT\"],[\"setPinMode\", \"analog\", 1,\"INPUT\"],[\"setPinMode\", \"digital\", 4,\"OUTPUT\"],[\"setPinMode\", \"analog\", 2,\"INPUT\"],[\"setPinMode\", \"digital\", 7,\"OUTPUT\"],[\"setPinMode\", \"analog\", 3,\"INPUT\"],[\"setPinMode\", \"digital\", 8,\"OUTPUT\"]");
- 
-cpf.uireset("[\"uiresetPin\"],[\"analog\", 0,\"INPUT\",\"Alcohol Sensor(A)\",\"img-alcohol\"],[\"digital\", 2,\"OUTPUT\",\"SPDT Relay \",\"img-spdt_relay\"],[\"digital\", 3,\"OUTPUT\",\"SPDT Relay \",\"img-spdt_relay\"],[\"analog\", 1,\"INPUT\",\"Light Sensor\",\"img-ext_lightsensor\"],[\"digital\", 4,\"OUTPUT\",\"SPDT Relay \",\"img-spdt_relay\"],[\"analog\", 2,\"INPUT\",\"Loudness Sensor\",\"img-loudness\"],[\"digital\", 7,\"OUTPUT\",\"SPDT Relay \",\"img-spdt_relay\"],[\"analog\", 3,\"INPUT\",\"Luminace Sensor\",\"img-luminanace\"],[\"digital\", 8,\"OUTPUT\",\"SPDT Relay \",\"img-spdt_relay\"]");
-}
-var SMS0, SMS1, SMS2, SMS3;
+
+var SMS0;
+var SMS1;
+var SMS2;
+var SMS3;
+ setup();
  
 function ALACOHOL_Sensor_Get() {
   var ALACOHOL_Sensor_GetValue = cpf.get("A0");
@@ -123,5 +122,12 @@ cpf.repeat();
  
  
 cpf.repeat();
+
+function setup(){
+		if(cpf)
+cpf.pinreset("[\"resetPin\"],[\"setPinMode\", \"analog\", 0,\"INPUT\"],[\"setPinMode\", \"digital\", 2,\"OUTPUT\"],[\"setPinMode\", \"digital\", 3,\"OUTPUT\"],[\"setPinMode\", \"analog\", 1,\"INPUT\"],[\"setPinMode\", \"digital\", 4,\"OUTPUT\"],[\"setPinMode\", \"analog\", 2,\"INPUT\"],[\"setPinMode\", \"digital\", 7,\"OUTPUT\"],[\"setPinMode\", \"analog\", 3,\"INPUT\"],[\"setPinMode\", \"digital\", 8,\"OUTPUT\"]");
+ 
+cpf.uireset("[\"uiresetPin\"],[\"analog\", 0,\"INPUT\",\"Alcohol Sensor(A)\",\"img-alcohol\"],[\"digital\", 2,\"OUTPUT\",\"SPDT Relay \",\"img-spdt_relay\"],[\"digital\", 3,\"OUTPUT\",\"SPDT Relay \",\"img-spdt_relay\"],[\"analog\", 1,\"INPUT\",\"Light Sensor\",\"img-ext_lightsensor\"],[\"digital\", 4,\"OUTPUT\",\"SPDT Relay \",\"img-spdt_relay\"],[\"analog\", 2,\"INPUT\",\"Loudness Sensor\",\"img-loudness\"],[\"digital\", 7,\"OUTPUT\",\"SPDT Relay \",\"img-spdt_relay\"],[\"analog\", 3,\"INPUT\",\"Luminace Sensor\",\"img-luminanace\"],[\"digital\", 8,\"OUTPUT\",\"SPDT Relay \",\"img-spdt_relay\"]");
+}
 
 //
